66401170ac9ddafdbe2b70d8cedbbedfcd8db178
luisrigor <luis.parra@rigorcg.pt>
2023-05-18 19:51:38 -0500

    update project

--- src/main/java/com/gsc/tvcmanager/TvcManagerApplication.java
+++ src/main/java/com/gsc/tvcmanager/TvcManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TvcmanagerApplication {
+public class TvcManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TvcmanagerApplication.class, args);
+		SpringApplication.run(TvcManagerApplication.class, args);
 	}
 
 }
