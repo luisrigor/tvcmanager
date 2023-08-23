3c3b4a5380e9bd0ca6fa2853cc552ba45a31b067
luisrigor <luis.parra@rigorcg.pt>
2023-08-23 17:40:20 -0500

    fix

--- src/main/java/com/gsc/tvcmanager/repository/toyota/PrevisionRepository.java
+++ src/main/java/com/gsc/tvcmanager/repository/toyota/PrevisionRepository.java
@@ -9,10 +9,6 @@ import java.util.List;
 
 public interface PrevisionRepository extends JpaRepository<TVCUsedCarsPrevisionSales, Integer>, PrevisionRepositoryCustom {
 
-//    @Query(value = "SELECT OID_DEALER, SUM(PREVISION_TVC) AS PREVISION_TVC, SUM(PREVISION_SN) AS PREVISION_SN FROM TVC_USED_CARS_PREVISION_SALES WHERE YEAR= :year AND MONTH <= :month AND PREVISION_TYPE= :previsionType  AND STATUS='Fechado' GROUP BY OID_DEALER" , nativeQuery = true)
-//    List<TVCUsedCarsPrevisionSales> previsionAnnual(@Param("year") Integer year,@Param("month") Integer month, @Param("reportType") String reportType, @Param("previsionType") String previsionType);
-
-
     @Query(value = "SELECT * FROM TVC_USED_CARS_PREVISION_SALES WHERE YEAR = :year AND MONTH = :month AND PREVISION_TYPE= :previsionType", nativeQuery = true)
     List<TVCUsedCarsPrevisionSales> previsionMonthly(@Param("year") Integer year,@Param("month") Integer month, @Param("previsionType") String previsionType);
     @Query(value = " SELECT * FROM TVC_USED_CARS_PREVISION_SALES WHERE YEAR= :year AND MONTH= :month AND PREVISION_TYPE= :previsionType AND STATUS='Fechado'",nativeQuery = true)
